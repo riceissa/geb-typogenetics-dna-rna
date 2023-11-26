b1154fd64fabd16142f6ded2d6d21cc5016dcad0
Issa Rice <riceissa@gmail.com>
2023-11-25 21:08:12 -0800

    more work

--- proc.py
+++ proc.py
@@ -12,18 +12,31 @@ print("""<!DOCTYPE html>
       <link rel="stylesheet" href="../style.css">
     </head>
     <body>
+    <div class="container">
     """)
 
 for note_id in col.find_notes(""):
     note = col.get_note(note_id)
     print('<div class="card">')
+
+    print('<div class="front">')
     print(note["Front"])
+    print("</div>")
+
     print("<hr/>")
+
+    print('<div class="back">')
     print(note["Back"])
+    print("</div>")
+
+    print('<div class="notes">')
     print(note["Notes"])
     print("</div>")
 
+    print("</div>")
+
 print("""
+        </div>
         </body>
         </html>
 """)
