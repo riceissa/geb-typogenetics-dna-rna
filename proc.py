1cdd27d0c323017669d5e5f9a3ec49374ed43c87
Issa Rice <riceissa@gmail.com>
2023-11-26 11:55:44 -0800

    make it print cards by section

--- proc.py
+++ proc.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+import sys
 from anki.collection import Collection
 
+import util
+
 col = Collection("apkg/collection.anki21")
+
+section_map = {}
+
+for section in util.sections:
+    section_map[section] = []
+
+for note_id in col.find_notes(""):
+    note = col.get_note(note_id)
+    try:
+        section_map[note["Section"]].append(note)
+    except KeyError:
+        print(f"Card has section name {note['Section']}, but this section isn't in the sections list!", file=sys.stderr)
+        sys.exit()
 
 print("""<!DOCTYPE html>
 <html lang="en">
@@ -15,25 +31,29 @@ print("""<!DOCTYPE html>
     <div class="container">
     """)
 
-for note_id in col.find_notes(""):
-    note = col.get_note(note_id)
-    print('<div class="card">')
+for section in util.sections:
+    print(f'<h2>{section}</h2>')
+    if not section_map[section]:
+        print("<p>There are no cards for this section.</p>")
+    for note in section_map[section]:
+        note = col.get_note(note_id)
+        print('<div class="card">')
 
-    print('<div class="front">')
-    print(note["Front"])
-    print("</div>")
+        print('<div class="front">')
+        print(note["Front"])
+        print("</div>")
 
-    print("<hr/>")
+        print("<hr/>")
 
-    print('<div class="back">')
-    print(note["Back"])
-    print("</div>")
+        print('<div class="back">')
+        print(note["Back"])
+        print("</div>")
 
-    print('<div class="notes">')
-    print(note["Notes"])
-    print("</div>")
+        print('<div class="notes">')
+        print(note["Notes"])
+        print("</div>")
 
-    print("</div>")
+        print("</div>")
 
 print("""
         </div>
