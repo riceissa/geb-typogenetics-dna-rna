ba7cf2238092ddba1a86546ed4fbf43856ffc57a
Issa Rice <riceissa@gmail.com>
2023-11-27 02:24:40 -0800

    count cards

--- orbit.py
+++ orbit.py
@@ -44,7 +44,8 @@ with open("docs/orbit/index.html", "w") as f:
         if not util.section_map[section]:
             f.write(f"<li>{section} (no cards for this section)</li>\n")
         else:
-            f.write(f'<li><a href="{util.slugify(section)}">{section}</a> ({len(util.section_map[section])} cards)</li>\n')
+            num_cards = len(util.section_map[section])
+            f.write(f'<li><a href="{util.slugify(section)}">{section}</a> ({num_cards} card{"" if num_cards == 1 else "s"})</li>\n')
 
             section_dir = f"docs/orbit/{util.slugify(section)}/"
             pathlib.Path(section_dir).mkdir(exist_ok=True)
